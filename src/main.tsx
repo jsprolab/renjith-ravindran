12264d3f12dcaf637dec4b9eb2de18ee3adeb660
Renjith <renjith@Renjiths-MacBook-Pro.local>
2025-09-03 22:50:05 -0700

    Fix main.tsx to import App instead of deleted TestApp

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import TestApp from './TestApp'
+import App from './App'
 import './index.css'
 
 import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <HelmetProvider>
-    <TestApp />
+    <App />
   </HelmetProvider>,
 )
