9a995bfe0ea914977a1c6814224779c5f0cfdced
Renjith <renjith@Renjiths-MacBook-Pro.local>
2025-09-04 00:16:27 -0700

    Add navigation and HomeCard sidebar to Resume page - complete layout consistency

--- src/pages/resume/index.tsx
+++ src/pages/resume/index.tsx
@@ -1,22 +1,59 @@
 import { useData } from '../../hooks/useData'
 import { ResumeCard } from './ResumeCard'
-import { Avatar, Footer, PageTitle } from '../../components'
+import { Avatar, Footer, PageTitle, DesktopNav, MobileNav, MenuButton, ThemeToggleButton, SocialLinks } from '../../components'
+import { HomeCard } from '../home/HomeCard'
 import ResumeDoc from '../../assets/resume/Renjith_Ravindran.pdf'
 import dowloadIcon from '../../assets/icons/download.png'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../../assets/logo/renjith.png'
 
 export const Resume = () => {
-  const { advanceSkills, familiarSkills, tools, avatars, handleImageChange } = useData()
+  const { advanceSkills, familiarSkills, tools, avatars, handleImageChange, setPrivacyPolicyIsOpen } = useData()
+  const [menuOpen, setMenuOpen] = useState(false);
 
   useEffect(() => {
     handleImageChange(false, avatars.greatness)
   }, [])
+
+  useEffect(() => {
+    if (menuOpen) {
+      setPrivacyPolicyIsOpen(false)
+    }
+  }, [menuOpen])
+
   return (
-    <>
+    <main className="relative">
       <PageTitle title="Resume"></PageTitle>
       {/* End pagetitle */}
 
-      <section className="bg-white overflow-hidden lg:mb-20 lg:rounded-2xl dark:bg-[#111111]">
+      <section className="z-[100] bg-white dark:bg-black min-h-screen bg-no-repeat bg-center bg-cover bg-fixed w-full">
+        <div className="container z-[1000] w-full bg-primary-bg-light dark:bg-primary-bg-dark lg:bg-transparent lg:dark:bg-transparent flex justify-between py-5 lg:px-0 lg:pt-[35px]">
+          <div className="w-full z-[1000] flex justify-between items-center px-4">
+            {/* website logo */}
+            <Link to="/">
+              <img className="h-[26px] lg:h-[32px] rounded-md hover:scale-[105%] transition-all duration-50 ease-in-out" src={logo} alt="logo" />
+            </Link>
+            <div className="flex items-center">
+              {/* dark and light mode button */}
+              <ThemeToggleButton />
+
+              {/* mobile menu button */}
+              <MenuButton menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+            </div>
+          </div>
+        </div>
+        <MobileNav menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <div className="container grid grid-cols-12 md:gap-10 justify-between lg:mt-[110px] ">
+          <div className="col-span-12 lg:col-span-4 hidden lg:block h-auto sticky">
+            <HomeCard />
+          </div>
+          <div className="col-span-12 lg:col-span-8  ">
+            {/* header  */}
+            <DesktopNav />
+
+            {/* Content */}
+            <section className="bg-white overflow-hidden lg:mb-20 lg:rounded-2xl dark:bg-[#111111]">
         <div className="relative w-full flex items-center justify-center">
           <div className="lg:hidden absolute z-10 top-[10rem] w-[200px]" data-aos="fade-in">
             <Avatar absolute={false} width="200px"/>
@@ -95,6 +132,12 @@ export const Resume = () => {
           </div>
         </div>
       </section>
-    </>
+      </div>
+      </div>
+      </section>
+      <div data-aos="fade-up" className='socialSm:fixed hidden socialSm:flex flex-col items-center bg-transparent button-vertical-after after:h-40 after:translate-y-[7rem] bottom-0 left-0 w-[10%] h-[25%]'>
+        <SocialLinks />
+      </div>
+    </main>
   )
 }
