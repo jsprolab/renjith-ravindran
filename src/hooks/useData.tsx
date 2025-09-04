1a73909c9ffdb7f0fd933bbda506eecf0e21f25b
Renjith <renjith@Renjiths-MacBook-Pro.local>
2025-09-03 22:41:40 -0700

    Fix useData hook to throw error when context is null - prevents silent failures

--- src/hooks/useData.tsx
+++ src/hooks/useData.tsx
@@ -3,5 +3,11 @@ import { useContext } from "react";
 import { Context } from "../context/ContextProvider";
 
 export const useData = () => {
-    return useContext(Context)
+    const context = useContext(Context);
+    
+    if (!context) {
+        throw new Error('useData must be used within a ContextProvider');
+    }
+    
+    return context;
 }
